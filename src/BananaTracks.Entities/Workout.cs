ca0a00267cadfc085e9652093671a9a1da4d3ce5
Andy Mehalick <andy.mehalick@orangejetpack.com>
2017-03-21 16:37:50 +0300

    add workout.duration

--- src/BananaTracks.Entities/Workout.cs
+++ src/BananaTracks.Entities/Workout.cs
@@ -8,5 +8,10 @@ namespace BananaTracks.Entities
         public Activity Activity { get; set; }
         public DateTime StartUtc { get; set; }
         public DateTime? EndUtc { get; set; }
+
+        /// <summary>
+        /// Gets a workout's duration in minutes.
+        /// </summary>
+        public double? Duration => EndUtc?.Subtract(StartUtc).TotalMinutes;
     }
 }
